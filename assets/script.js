ccacbfa4b6935f89174d6fb5fd549d571b90743f
Nate LaBine <njlabine@gmail.com>
2022-05-17 13:26:56 -0400

    saving changes

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// radomize movie quote when clicked
+
+function quote() {
+  const APIKey = "bearer EG4f6juh1PF0X82amiv_";
+  fetch("https://the-one-api.dev/v2/quote", {
+    headers: { Authorization: "bearer EG4f6juh1PF0X82amiv_" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      for (var index = 0; index < data["docs"].length; index++) {
+        console.log(data["docs"][index]["dialog"]);
+      }
+    });
+}
