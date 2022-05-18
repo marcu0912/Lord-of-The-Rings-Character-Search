f495328513cbdc1201deb24ba385bb8627f992bf
Nate LaBine <njlabine@gmail.com>
2022-05-18 16:45:17 -0400

    added the randomize feature that toggles through quotes

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,22 @@
 
 var quoteDisplayEl = document.getElementById("movie-quote-container");
 
+function randomIndex() {
+  return Math.floor(Math.random() * 1000);
+}
+
 function quote() {
+  quoteDisplayEl.innerHTML = "";
+
   fetch("https://the-one-api.dev/v2/quote", {
     headers: { Authorization: "bearer EG4f6juh1PF0X82amiv_" },
   })
     .then((response) => response.json())
     .then((data) => {
-      for (var index = 0; index < data["docs"].length; index++) {
-        console.log(data["docs"][index]["dialog"]);
-        var quotes = document.createElement("div");
-        quotes.textContent = data["docs"][index]["dialog"];
-        quoteDisplayEl.appendChild(quotes);
-      }
+      var singleQuote = data["docs"][randomIndex()];
+      var quotes = document.createElement("div");
+      quotes.textContent = singleQuote.dialog;
+      quoteDisplayEl.appendChild(quotes);
     });
 }
 
