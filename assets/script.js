8d2389d80457e36e4e87c776155bc2ca7f2a9e54
Ivan Goya <ivan.goya98@gmail.com>
2022-05-18 13:36:33 -0400

    commiting to pull new origin

--- assets/script.js
+++ assets/script.js
@@ -16,16 +16,20 @@ function quote() {
 }
 
 
-    // 3 side character buttons
-legolasEl.addEventListener("dblclick", function() {
+// 3 side character buttons
+legolasEl.addEventListener("dblclick", function () {
   fetch("https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfd81", {
     headers: { Authorization: "bearer EG4f6juh1PF0X82amiv_" },
   })
-     .then((response) => response.json())
-     .then((data) => {
-        console.log(data["docs"]["race"]);
-})
-    
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data["docs"]["name"]);
+      console.log(data["docs"]["race"]);
+      console.log(data["docs"]["hair"]);
+      console.log(data["docs"]["death"]);
+    })
+}
+
 
 
 document.getElementById("ring-title").addEventListener("click", elvishFont);
@@ -33,6 +37,7 @@ function elvishFont() {
   var selection = document.getElementById("body");
   if (selection.style.fontFamily == 'Bilbo') {
     selection.style.fontFamily = "elvish_ring_nfiregular";
-  }else{
+  } else {
     selection.style.fontFamily = "Bilbo";
   }
+}
